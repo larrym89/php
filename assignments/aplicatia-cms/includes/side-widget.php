cb6c1275a7ef2f48e1d4d933b7e31da09ac5da01
larrym89 <maholeal89@gmail.com>
2023-04-09 19:16:13 +0200

    final update

--- assignments/aplicatia-cms/includes/side-widget.php
+++ assignments/aplicatia-cms/includes/side-widget.php
@@ -6,7 +6,6 @@
                 <ul class="list-unstyled mb-0">
                    
                             <li><a href="add-news.php" class="text-decoration-none">Add news</a></li>
-                            <li><a href="add-category.php" class="text-decoration-none">Add category</a></li>
                      
                 </ul>
             </div>
